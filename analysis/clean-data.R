d2f241fd3e05fbaede5cff4c901a2adbcdac7914
NettleHook <catharina.castillo@gmail.com>
2025-09-23 17:34:59 -0700

    Proofread paper and added figs. Moved all analysis code to be inline with the paper.
    Completed data cleaning.
    
    Still need to finish abstract and try to reduce wordiness of paper

--- analysis/clean-data.R
+++ analysis/clean-data.R
@@ -14,8 +14,7 @@ ggplot(water_raw, aes(x = TOTAL_WATER_USE, y = WUS_URBAN)) +
 
 #no missing values, but there is at least one entry where total water use == 0
 water_cleaning <- water_raw %>%
-  filter(TOTAL_WATER_USE != 0) %>%
-  droplevels()
+  filter(TOTAL_WATER_USE != 0)
 summary(water_cleaning)
 
 #also noted from graph that it appears may be some values where it looks like TOTAL_WATER_USE is less than WUS_AGRICULTURAL or WUS_URBAN
@@ -36,11 +35,22 @@ water_cleaning_with_check <- water_cleaning %>%
     total = total_water_and_other - sum_used_up
   )
 no_usage <- water_cleaning_with_check %>%
-  filter(sum_used_up == 0) %>%
-  droplevels()
-#repeat offenders-- need to dig in
+  filter(sum_used_up == 0)
+#repeat offenders-- need to dig in. Compare with groundwater extraction database, as that sometimes has entries
 
-water_clean <- anti_join(water_cleaning_with_check, no_usage, by = c("SUBBASIN_NUMBER", "REPORT_YEAR"))
+groundwater <- read.csv("./data/groundwater_extraction.csv") %>%
+  filter(SUBBASIN_NUMBER %in% no_usage$SUBBASIN_NUMBER)
+
+#BIG VALLEY is only one that also has empty entries in both datasets--likely unfinished report
+
+#however, the groundwater database only reports groundwater used in each sector, not all water
+
+#We have some strong outliers in the urban sections. Limit decided by examining scatter plot
+urban_outliers <- filter(water_cleaning_with_check, WUS_URBAN >= 200000 & WUS_AGRICULTURAL < 150000)
+
+water_cleaning_no_urban_outliers <- filter(water_cleaning_with_check, SUBBASIN_NUMBER != "8-001") 
+
+water_clean <- anti_join(water_cleaning_no_urban_outliers, no_usage, by = c("SUBBASIN_NUMBER", "REPORT_YEAR"))
 
 write.csv(water_clean, './data/total_water_use_clean.csv')
           
